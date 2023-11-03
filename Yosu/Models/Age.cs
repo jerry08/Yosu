5f6ca1e9a6c1a4e9f5a2af3c704a54352b22cb46
jerrry08 <jeremyyearwood99@gmail.com>
2023-11-03 13:31:38 -0400

    Add models

---
+++ Yosu/Models/Age.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Yosu.Models;
+
+public class Age
+{
+    public int Years;
+    public int Months;
+    public int TotalMonths;
+    public int Weeks;
+    public int Days;
+    public int Hours;
+    public int Minutes;
+    public int Seconds;
+
+    public Age(DateTime Bday)
+    {
+        Count(Bday);
+    }
+
+    public Age Count(DateTime bday)
+    {
+        var timeSpan = DateTime.Now - bday;
+        var days = timeSpan.Days;
+        if (timeSpan.Days <= 0)
+            days = 0;
+
+        var age = DateTime.MinValue.AddDays(days);
+
+        Years = age.Year - 1;
+        Months = age.Month - 1;
+        Days = age.Day - 1;
+        Hours = timeSpan.Hours;
+        Minutes = timeSpan.Minutes;
+        Seconds = timeSpan.Seconds;
+
+        TotalMonths = Months + (Years * 12);
+
+        return this;
+    }
+}
