e89e54c5d6966e489de182e054585d0ded798c9c
jerry08 <jeremyyearwood99@gmail.com>
2024-07-13 13:24:28 -0400

    Closes #4

--- Yosu/Services/PreferenceService.cs
+++ Yosu/Services/PreferenceService.cs
@@ -1,23 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Text.Json.Serialization;
 using Cogwheel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Storage;
-using PropertyChanged;
 using Yosu.ViewModels.Components;
 
 namespace Yosu.Services;
 
-[AddINotifyPropertyChangedInterface]
-public partial class PreferenceService : SettingsBase, INotifyPropertyChanged
+// Can't use [ObservableProperty] here because System.Text.Json's source generator doesn't see
+// the generated properties.
+[INotifyPropertyChanged]
+public partial class PreferenceService()
+    : SettingsBase(
+        Path.Combine(FileSystem.AppDataDirectory, "Preferences.json"),
+        SerializerContext.Default
+    )
 {
-    public AppTheme AppTheme { get; set; }
+    private AppTheme _appTheme;
+    public AppTheme AppTheme
+    {
+        get => _appTheme;
+        set => SetProperty(ref _appTheme, value);
+    }
 
-    public List<DownloadItem> Downloads { get; set; } = [];
+    private List<DownloadItem> _downloads = [];
+    public List<DownloadItem> Downloads
+    {
+        get => _downloads;
+        set => SetProperty(ref _downloads, value);
+    }
 
-    public SourceType SearchSourceType { get; set; } = SourceType.Youtube;
+    private SourceType _searchSourceType = SourceType.Youtube;
+    public SourceType SearchSourceType
+    {
+        get => _searchSourceType;
+        set => SetProperty(ref _searchSourceType, value);
+    }
+}
 
-    public PreferenceService()
-        : base(Path.Combine(FileSystem.AppDataDirectory, "Preferences.json")) { }
+public partial class PreferenceService
+{
+    [JsonSerializable(typeof(PreferenceService))]
+    private partial class SerializerContext : JsonSerializerContext;
 }
